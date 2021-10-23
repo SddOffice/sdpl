fd0d286f1df0ca00e9dba19d827a8012f1c85c84
SddOffice <ssdoffice44@gmail.com>
2021-10-23 12:13:17 +0530

    Initial Commit

---
+++ app/Http/Controllers/RegistrationController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Registration;
+
+class RegistrationController extends Controller
+{
+    //
+    public function index()
+    {
+        $data = Registration::all();
+        return response()->json([
+            'status'=>200,
+            'msg'=>'ok'
+        ]);
+    }
+}
